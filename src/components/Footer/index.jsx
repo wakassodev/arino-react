6182f215e12d6be464fa7735a6ff3c57edc07d2d
wakassodev <wakasso94@gmail.com>
2023-11-22 12:17:17 +0400

    Updating the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -78,7 +78,7 @@ export default function Footer({copyrightText, logoSrc, logoAlt, text}) {
       <Div className="container">
         <Div className="cs-bottom_footer">
           <Div className="cs-bottom_footer_left">
-            <Div className="cs-copyright">Copyright © 2022 Laralink.</Div>
+            <Div className="cs-copyright">Copyright © 2023 wakasso.</Div>
           </Div>
           <Div className="cs-bottom_footer_right">
             <MenuWidget menuItems={copyrightLinks} variant=' cs-style2'/>
